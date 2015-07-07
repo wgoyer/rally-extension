6c8f60ae22ae06258ddc3960701bbfcb3f4a7669
Rally Dev Pair <pairing@rallydev.com>
2015-07-07 16:51:13 -0600

    Uncommented some stuff in oldCode.js since it's unused.

--- oldCode.js
+++ oldCode.js
@@ -1,38 +1,38 @@
 	//In start extensions in bg.js
-	// var changeUrl = "";
-	// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabState){
-	// 	if(changeInfo.status === "loading" && changeInfo.url) changeUrl = changeInfo.url;
-	// 	if(changeInfo.status === "complete" && changeUrl !== ""){
-	// 		if(changeUrl.substring(8, settings.domain.length+8) === settings.domain){
-	// 			changeUrl = "";
-	// 			return checkURLforArtifactInfo(tabState);
-	// 		};
-	// 	}	
-	// });
+	var changeUrl = "";
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabState){
+		if(changeInfo.status === "loading" && changeInfo.url) changeUrl = changeInfo.url;
+		if(changeInfo.status === "complete" && changeUrl !== ""){
+			if(changeUrl.substring(8, settings.domain.length+8) === settings.domain){
+				changeUrl = "";
+				return checkURLforArtifactInfo(tabState);
+			};
+		}	
+	});
 	
 	
 	// tabs execute script under pullArtifactAndStoreToRecents bg.js: 
-	// var waitForDOMAndPullTitle = 'document.addEventListener("DOMContentLoaded", function(){document.title;});'
+	var waitForDOMAndPullTitle = 'document.addEventListener("DOMContentLoaded", function(){document.title;});'
 
-	// chrome.tabs.executeScript(tab.id, {file : './injection/scrapeFromDetailPage.js'}, function(result){
-	// 	console.log(result);
-	// });
+	chrome.tabs.executeScript(tab.id, {file : './injection/scrapeFromDetailPage.js'}, function(result){
+		console.log(result);
+	});
 	
 	
 	//This is stupid.  Apparently onUpdate does not update the tab's title when the "complete" update is called, so we have to re-query the tab to get current title.
 	
-	// setTimeout(function(){
-	// 	chrome.tabs.get(tab.id, function(tabData){
-	// 		var recents = {},
-	// 			currentIndex = 0,
-	// 			currentItem = {"Title" : tabData.title, "URL" : tabData.url};
-	// 		if(localStorage['rally-ext-recents']){
-	// 			recents = JSON.parse(localStorage['rally-ext-recents']);
-	// 			currentIndex = recents.recentlyVisited.indexOf(currentItem);
-	// 			if(currentIndex !== -1){
-	// 				recents.recentlyVisisted.splice(currentIndex, 1)
-	// 			}
-	// 		} 
-	// 		return recents.recentlyVisited.unshift(currentItem);
-	// 	});
-	// },5000);
+	setTimeout(function(){
+		chrome.tabs.get(tab.id, function(tabData){
+			var recents = {},
+				currentIndex = 0,
+				currentItem = {"Title" : tabData.title, "URL" : tabData.url};
+			if(localStorage['rally-ext-recents']){
+				recents = JSON.parse(localStorage['rally-ext-recents']);
+				currentIndex = recents.recentlyVisited.indexOf(currentItem);
+				if(currentIndex !== -1){
+					recents.recentlyVisisted.splice(currentIndex, 1)
+				}
+			} 
+			return recents.recentlyVisited.unshift(currentItem);
+		});
+	},5000);
